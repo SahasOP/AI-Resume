395bd363b059cbe3812e18d0dd6258ba2b009655
SahasOP <sahasprajapati54@gmail.com>
2024-07-13 20:31:01 +0530

    first

---
+++ src/main.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SignInPage from "./auth/sign-in/index.jsx";
+import Home from "./home/index.jsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+import Dashboard from "./dashboard/index.jsx";
+import EditResume from "./dashboard/resume/edit/index.jsx";
+import ViewResume from "./my-resume/[resumeId]/view/index.jsx";
+import ErrorBoundary from "./ErrorBoundary.jsx";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard />
+      },
+      {
+        path: "/dashboard/resume/:resumeId/edit",
+        element: <EditResume />
+      }
+    ],
+  },
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/auth/sign-in",
+    element: <SignInPage />,
+  },
+  {
+    path: "/my-resume/:resumeId/view",
+    element: <ViewResume />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <RouterProvider router={router} />
+      </ClerkProvider>
+    </ErrorBoundary>
+  </React.StrictMode>
+);
